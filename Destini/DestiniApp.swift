8e580b697636cd5315606b402a5e21a93f7ac932
Alisher <Alisher>
2021-10-27 12:57:22 +0600

    Initial Commit

---
+++ Destini/DestiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestiniApp.swift
+//  Destini
+//
+//  Created by Алишер Алсейт on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DestiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
